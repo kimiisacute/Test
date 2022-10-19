ac03b337ccc637eda7fc669f13605a13d613c49e
xunguicong <1130719745@qq.com>
2022-10-19 21:34:18 +0800

    首页

---
+++ unny/src/main/java/com/example/unny/activity/GrzxActivity.java
@@ -0,0 +1,61 @@
+package com.example.unny.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.example.unny.R;
+
+public class GrzxActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener{
+    //页面控件
+    ImageView iv_fh;
+    ImageView wo_xy;
+    ImageView wo_xy1;
+    private Switch wo_xy2;
+    ImageView wo_xy3;
+    ImageView wo_xy4;
+    ImageView wo_xy5;
+    ImageView wo_xy6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grzx);
+        //获取控件
+        iv_fh=findViewById(R.id.iv_fh);
+        wo_xy=findViewById(R.id.wo_xy);
+        wo_xy1=findViewById(R.id.wo_xy1);
+        wo_xy2=(Switch) findViewById(R.id.wo_xy2);
+        wo_xy3=findViewById(R.id.wo_xy3);
+        wo_xy4=findViewById(R.id.wo_xy4);
+        wo_xy5=findViewById(R.id.wo_xy5);
+        wo_xy6=findViewById(R.id.wo_xy6);
+
+        wo_xy2.setOnCheckedChangeListener(this);
+
+        iv_fh.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void  onClick(View v){
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if(isChecked){
+            Toast.makeText(GrzxActivity.this,"",Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(GrzxActivity.this,"",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
